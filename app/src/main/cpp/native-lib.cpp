775a3400eef25cbace1e58a5210cddec22cf8985
krokozabra <gadkondrad.zveruga@mail.ru>
2022-03-29 16:57:09 +0300

    add nlohmann

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,8 @@
 #include <jni.h>
 #include <string>
+#include "json.hpp"
 
+using json = nlohmann::json;
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_kondaurov_ndktest_MainActivity_stringFromJNI(
         JNIEnv* env,
